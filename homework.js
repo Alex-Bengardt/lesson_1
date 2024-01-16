45adfda748cb39e120169ec175f34f24e96e3d7b
Alex Bengardt <alexandragorky@yandex.ru>
2024-01-16 12:16:39 +0300

    first commit

---
+++ homework.js
@@ -0,0 +1,8 @@
+let userName = 'Jack';
+let userAge = 29;
+let married = false;
+console.log(`Имя: ${userName}.`);
+console.log(`Возраст: ${userAge} лет.`);
+console.log(`Есть супруг/супруга: ${married}.`);
+
+console.log("Тип данных переменных userName, userAge, married: " + typeof(userName), typeof(userAge), typeof(married));
